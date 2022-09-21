1fabc9e44ef66f9f3b5eef1b1f6d2b6b96c6debe
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-09-21 17:59:19 +0000

    Added the elements for each section. still need css/bootstrap.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,19 @@
 spaceBun = {
-    page0 : 'Hello this will let me read mind',
+    page0 : 'Hello this will let me read your mind',
     page1 : 'Haha I read your mind... wait',
     page2 : 'No you beat me'
 }
-let bodyBack = document.querySelector('body')
-bodyBack.setAttribute('class', 'p-3 mb-2 bg-success text-white')
-
-let header = document.getElementById('headerText');
+let bodyBack = document.querySelector('body');
+bodyBack.setAttribute('class','p-3 mb-2 bg-success text-white');
+bodyBack.setAttribute('id', 'bodyTag')
+// Added a class and tried to getElementByClass, wouldn't work but I was able to add an ID and that work
+let containerTag = document.getElementById('bodyTag');
+let containerArea = document.createElement('div');
+containerTag.appendChild(containerArea)
+containerArea.setAttribute('Class', 'container')
+containerArea.setAttribute('id', 'boxStop')
+containerArea.innerText = 'affadsfasdfdf'
+let header = document.getElementById('boxStop');
 let h1Section = document.createElement('h1');
 header.appendChild(h1Section);
 h1Section.innerText=spaceBun.page0;
@@ -14,9 +21,9 @@ h1Section.setAttribute('id', 'h1ID')
 let changeUp = document.getElementById('h1ID')
 let newButton = document.createElement('button')
 header.appendChild(newButton);
-newButton.innerText = 'Click';
-newButton.setAttribute('id', 'buttonOne');
-clickButton = document.getElementById('buttonOne');
+newButton.innerText = 'Next';
+newButton.setAttribute('id', 'nextButton');
+clickButton = document.getElementById('nextButton');
 clickButton.addEventListener('click',() => {
     console.log('hi')
     if (changeUp.innerText === spaceBun.page0) {
@@ -30,3 +37,21 @@ else if (changeUp.innerText === spaceBun.page1) {
 else {h1Section.innerText = 'broke'}
 }
 )
+let helpArea = document.getElementById('boxStop');
+let helpTag = document.createElement('div');
+helpArea.appendChild(helpTag);
+helpTag.innerText = 'Inner Text is working';
+helpTag.setAttribute('Id', 'helpText')
+let extraArea = document.getElementById('boxStop');
+let extraTag = document.createElement('div');
+extraArea.appendChild(extraTag);
+extraTag.innerText='extra text here';
+extraTag.setAttribute('Id', 'extraText')
+let circleArea = document.getElementById('boxStop');
+let circleTag = document.createElement('button');
+circleArea.appendChild(circleTag);
+circleTag.setAttribute('Id','resetGo');
+let circleButton = document.getElementById('resetGo');
+circleButton.innerText='Go or Reset'
+
+
